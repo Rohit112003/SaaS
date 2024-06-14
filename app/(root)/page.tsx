03fab71ab2926620a635c8b659690c88b279f0fc
Rohit Kumar <rohitK79739@gmail.com>
2024-06-14 22:06:42 +0530

    Initial Commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignInButton, UserButton } from '@clerk/nextjs'
+const page = () => {
+  return (
+    <div>
+      <p>Home</p>
+     
+    </div>
+  )
+}
+
+export default page
---
+++ app/(root)/transformation/add/[type]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddTRansformationTypePage = () => {
+  return (
+    <div>AddTRansformationTypePage</div>
+  )
+}
+
+export default AddTRansformationTypePage
